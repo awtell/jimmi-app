54fc79dd80efdcc271cbb62ef0799d021689bfdf
awtel <97784287+awtell@users.noreply.github.com>
2024-06-09 13:03:31 +0300

    updating bootstrap logos

--- src/components/HomePage/Components/TableList/RoomForm.tsx
+++ src/components/HomePage/Components/TableList/RoomForm.tsx
@@ -32,26 +32,30 @@ const RoomForm = React.forwardRef<HTMLDivElement, RoomFormProps>((props, ref) =>
           <h2>Available Tables</h2>
           <p>Book Your Table Now</p>
         </div>
-        <div className="room-cards-container">
-          {[...Array(3)].map((_, index) => (
-            <div className="room-card" key={index} onClick={handleBooking}>
-              <div className="image-container">
-                <img src={im1} alt="Room" className="room-image" />
-                <div className="overlay">
-                  <div className="overlay-text">
-                    <div className="room-number">Table {formData.number}</div>
-                    <div className="room-capacity">Capacity: {formData.capacity}</div>
-                    <div className="room-price">{formData.pricePerHour} per hour</div>
+        <div className="row">
+
+          <div className="room-cards-container">
+            {[...Array(5)].map((_, index) => (
+              <div className="room-card" key={index} onClick={handleBooking}>
+                <div className="image-container">
+                  <img src={im1} alt="Room" className="room-image" />
+                  <div className="overlay">
+                    <div className="overlay-text">
+                      <div className="room-number">Table {formData.number}</div>
+                      <div className="room-capacity">Capacity: {formData.capacity}</div>
+                      <div className="room-price">{formData.pricePerHour} per hour</div>
+                    </div>
                   </div>
                 </div>
+                <button className="book-button" onClick={handleBooking}>Book Now</button>
               </div>
-              <button className="book-button" onClick={handleBooking}>Book Now</button>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
 
-    </section>
+
+    </section >
   );
 });
 
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,12 +2,12 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
 import logo from '../../assets/log.png';
+import '../../assets/vendor/bootstrap/css/bootstrap.min.css';
+import '../../assets/vendor/boxicons/css/boxicons.min.css';
+import '../../assets/vendor/glightbox/css/glightbox.min.css';
+import '../../assets/vendor/swiper/swiper-bundle.min.css';
+import '../../assets/vendor/bootstrap-icons/bootstrap-icons.css';
 
-// import boxicons from '../../assets/bootstrap-icons/fonts/boxicons.min.css';
-// import '../../../assets/bootstrap-icons/fonts/bootstrap-icons.css';
-// import '../../../assets/vendor/boxicons/css/boxicons.min.css';
-// import '../../../assets/vendor/glightbox/css/glightbox.min.css';
-// import '../../assets/vendor/swiper/swiper-bundle.min.css';
 
 const NavBar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -48,27 +48,8 @@ const NavBar = () => {
             <li><a className="nav-link scrollto" href="#about">About Us</a></li>
             <li><a className="nav-link scrollto" href="#services">Services</a></li>
             <li><a className="nav-link scrollto " href="#portfolio">Contact Us</a></li>
-
-            {/* <li className="dropdown"><a href="#"><span>Drop Down</span> <i className="bi bi-chevron-down"></i></a>
-              <ul>
-                <li><a href="#">Drop Down 1</a></li>
-                <li className="dropdown"><a href="#"><span>Deep Drop Down</span> <i className="bi bi-chevron-right"></i></a>
-                  <ul>
-                    <li><a href="#">Deep Drop Down 1</a></li>
-                    <li><a href="#">Deep Drop Down 2</a></li>
-                    <li><a href="#">Deep Drop Down 3</a></li>
-                    <li><a href="#">Deep Drop Down 4</a></li>
-                    <li><a href="#">Deep Drop Down 5</a></li>
-                  </ul>
-                </li>
-                <li><a href="#">Drop Down 2</a></li>
-                <li><a href="#">Drop Down 3</a></li>
-                <li><a href="#">Drop Down 4</a></li>
-              </ul>
-            </li>
-            <li><a className="nav-link scrollto" href="#contact">Contact</a></li> */}
           </ul>
-          {/* <i className="bi bi-list mobile-nav-toggle" onClick={toggleMenu}></i> */}
+          <i className="bi bi-list mobile-nav-toggle" onClick={toggleMenu}></i>
         </nav>
 
         <div className="header-social-links d-flex align-items-center">
